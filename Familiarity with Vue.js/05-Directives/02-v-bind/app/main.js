9dc0acf9dce31043f73425b6e10498e2a14c037f
hmolhem <hosseinmolhem@gmail.com>
2022-03-18 23:27:58 +0330

    using directives in vuejs project v02

---
+++ Familiarity with Vue.js/05-Directives/02-v-bind/app/main.js
@@ -0,0 +1,12 @@
+Vue.createApp({
+    data(){
+        return{
+            title:'V-bind',
+            src: 'https://vuejs.org',
+            image: 'https://picsum.photos/200'
+        }
+    },
+    methdos:{
+
+    }
+}).mount('#app');
