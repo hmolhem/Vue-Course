7ca442a0549791976b71f3f4d3eb871393af5fc7
hmolhem <hosseinmolhem@gmail.com>
2022-03-19 11:27:56 +0330

    using directives in vuejs project v07

---
+++ Familiarity with Vue.js/06-Directives/03-v-else-if/app/main.js
@@ -0,0 +1,11 @@
+Vue.createApp({
+    data(){
+        return{
+            title: 'v-else-if',
+            number: 10,
+            msg1: 'سلام دنیا!',
+            msg2: 'نا برده رنج گنج میسر نمی شود',
+            msg3: 'من عاشق فریمورک ویو جی‌اس هستم.'
+        }
+    }
+}).mount('#app');
