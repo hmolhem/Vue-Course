307cb73e6d2f2659ffce91eb46e1f5e89e5778dc
hmolhem <hosseinmolhem@gmail.com>
2022-03-19 10:20:44 +0330

    using directive in vuejs project v03

---
+++ Familiarity with Vue.js/05-Directives/03-v-text/app/main.js
@@ -0,0 +1,12 @@
+Vue.createApp({
+    data(){
+        return{
+            title:'V-text',
+            englishMsg: 'No pain No gain',
+            persianMsg: 'نابرده رنج گنج میسر نمی‌شود',
+        }
+    },
+    methdos:{
+
+    }
+}).mount('#app');
