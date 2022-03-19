42511cc8f87dcb45cde242e114581b2258ad89d1
hmolhem <hosseinmolhem@gmail.com>
2022-03-19 11:12:47 +0330

    using directives in vuejs v06

---
+++ Familiarity with Vue.js/06-Directives/02-v-else/app/main.js
@@ -0,0 +1,10 @@
+Vue.createApp({
+    data(){
+        return{
+            title: 'v-else',
+            number: 8,
+            msg1: 'سلام دنیا!',
+            msg2: 'نا برده رنج گنج میسر نمی شود'
+        }
+    }
+}).mount('#app');
