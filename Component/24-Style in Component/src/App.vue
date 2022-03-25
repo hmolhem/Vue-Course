557f341d187102d79a8277f3bcccc95d1fef869a
hmolhem <hosseinmolhem@gmail.com>
2022-03-26 01:15:10 +0430

    Style and scoped in vuejs component

---
+++ Component/24-Style in Component/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <HelloWorld />
+  <p v-text="name"></p>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data(){
+    return{
+      name: 'Hossein Molhem'
+    }
+  }
+}
+</script>
+
+<style>
+p{
+  text-align: center;
+  background-color: blue;
+  color: aliceblue;
+  padding: 10px 0px;
+  font-size: 20px;
+  font-weight: bolder;
+}
+</style>
---
+++ Component/24-Style in Component/src/components/HelloWorld.vue
@@ -0,0 +1,25 @@
+<template>
+    <h1 v-text="title"></h1>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data(){
+    return{
+      title: 'This is a first vue CLI project'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1{
+  background-color: red;
+  color: aliceblue;
+  padding: 15px 0px;
+  border-radius: 5px;
+  text-align: center;
+}
+</style>
